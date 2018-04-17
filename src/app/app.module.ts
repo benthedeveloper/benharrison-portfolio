f151b34b375668500b33b9d7daef5ac23429c5dd
Ben Harrison <benjaminpharrison@live.com>
2018-04-17 11:48:12 -0700

    Create a custom module for all angular-material modules used.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
+// Modules
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomAngularMaterialModule } from './custom-angular-material.module';
 
-
+// Components
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

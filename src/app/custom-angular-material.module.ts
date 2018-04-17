8dd1ec8d7bacfb1e7867827b38bd0dff50be7d2f
Ben Harrison <benjaminpharrison@live.com>
2018-04-17 13:54:31 -0700

    Set up main nav links styling.

--- src/app/custom-angular-material.module.ts
+++ src/app/custom-angular-material.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class CustomAngularMaterialModule { }

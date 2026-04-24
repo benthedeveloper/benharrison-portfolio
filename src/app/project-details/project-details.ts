06b089780b6d09464d3d199943f71ae08aa0b5d1
Ben Harrison <benthedeveloper@users.noreply.github.com>
2026-04-24 01:16:47 -0700

    feat: Install Font Awesome, add icon to Try it out link

--- src/app/project-details/project-details.ts
+++ src/app/project-details/project-details.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DatePipe } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faDesktop } from '@fortawesome/free-solid-svg-icons';
 
 import { DataService } from '../data-service';
 import { Project } from '../models/project';
@@ -8,12 +10,13 @@ import { SiteHeader } from '../site-header/site-header';
 
 @Component({
   selector: 'app-project-details',
-  imports: [DatePipe, SiteHeader],
+  imports: [DatePipe, SiteHeader, FontAwesomeModule],
   templateUrl: './project-details.html',
   styleUrl: './project-details.scss',
 })
 export class ProjectDetails {
   project: Project | undefined;
+  faDesktop = faDesktop;
 
   constructor(
     private route: ActivatedRoute,
